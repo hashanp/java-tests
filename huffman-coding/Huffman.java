fd26e686536aba0495c16428f0ddb8e29f168a7e
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-06 18:09:51 +0100

    Adds code for Huffman Coding

--- huffman-coding/Huffman.java
+++ huffman-coding/Huffman.java
@@ -1,47 +1,61 @@
 public class Huffman {
 
-	private HuffmanData[] leafEntries;
-	private final static int SIZE = 50;
-	private PriorityQueueInterface<BinaryTreeInterface<HuffmanData>> pq;
-	private BinaryTreeInterface<HuffmanData> huffmanTree;
+  private HuffmanData[] leafEntries;
+  private final static int SIZE = 50;
+  private PriorityQueueInterface<BinaryTreeInterface<HuffmanData>> pq;
+  private BinaryTreeInterface<HuffmanData> huffmanTree;
 
-	public Huffman() {
-		leafEntries = new HuffmanData[SIZE];
-		pq = new PriorityQueue<BinaryTreeInterface<HuffmanData>>();
-		huffmanTree = new BinaryTree<HuffmanData>();
-	}
+  public Huffman() {
+    leafEntries = new HuffmanData[SIZE];
+    pq = new PriorityQueue<>();
+    huffmanTree = new BinaryTree<>();
+  }
 
-	public void setFrequencies() {
-	// Create test data and store as an array of HuffData
-		leafEntries[0] = new HuffmanData(5000, 'a');
-		leafEntries[1] = new HuffmanData(2000, 'b');
-		leafEntries[2] = new HuffmanData(10000, 'c');
-		leafEntries[3] = new HuffmanData(8000, 'd');
-		leafEntries[4] = new HuffmanData(22000, 'e');
-		leafEntries[5] = new HuffmanData(49000, 'f');
-		leafEntries[6] = new HuffmanData(4000, 'g');
-	}
+  public void setFrequencies() {
+  // Create test data and store as an array of HuffData
+    leafEntries[0] = new HuffmanData(5000, 'a');
+    leafEntries[1] = new HuffmanData(2000, 'b');
+    leafEntries[2] = new HuffmanData(10000, 'c');
+    leafEntries[3] = new HuffmanData(8000, 'd');
+    leafEntries[4] = new HuffmanData(22000, 'e');
+    leafEntries[5] = new HuffmanData(49000, 'f');
+    leafEntries[6] = new HuffmanData(4000, 'g');
+  }
 
-	public void setPriorityQueue() {
-	// Copy test data from array LeafEntries of HuffData
-	// into a PriorityQueue of HuffmanTree
-		for (int i = 0; i < 7; i++) {
-			if (leafEntries[i].getFrequency() > 0)
-				pq.add(new BinaryTree<HuffmanData>(leafEntries[i]));
-		}
-	}
+  public void setPriorityQueue() {
+  // Copy test data from array LeafEntries of HuffData
+  // into a PriorityQueue of HuffmanTree
+    for (int i = 0; i < 7; i++) {
+      if (leafEntries[i].getFrequency() > 0)
+        pq.add(new BinaryTree<>(leafEntries[i]));
+    }
+  }
 
-	public void createHuffmanTree() {
-	// Process PriorityQueue pq until there is a complete HuffmanTree
-	// ADD YOUR CODE HERE
-	}
+  public void createHuffmanTree() {
+    while (pq.getSize() != 1) {
+      final BinaryTree<HuffmanData> left
+          = (BinaryTree<HuffmanData>) pq.removeMin();
+      final BinaryTree<HuffmanData> right
+          = (BinaryTree<HuffmanData>) pq.removeMin();
+      final HuffmanData leftData = left.getRootData();
+      final HuffmanData rightData = right.getRootData();
+      final int freq = leftData.getFrequency() + rightData.getFrequency();
+      HuffmanData huffmanData = new HuffmanData(freq, ' ');
+      pq.add(new BinaryTree<>(huffmanData, left, right));
+    }
+    huffmanTree = pq.removeMin();
+  }
 
-	public void printCode() {
-	// ADD YOUR CODE HERE. 
-	}
+  public void printCode() {
+    printCodeProcedure("", huffmanTree);
+  }
 
-	private void printCodeProcedure(String code, BinaryTreeInterface<HuffmanData> tree) {
-	// Print out a complete HuffmanTree 
-        // ADD YOUR CODE HERE
-	}
+  private void printCodeProcedure(String code, BinaryTreeInterface<HuffmanData> tree) {
+    if (tree.getLeftSubtree() == null) {
+      System.out.println(tree.getRootData().getSymbol() + ": " + code);
+    } else {
+      printCodeProcedure(code + "0", tree.getLeftSubtree());
+      printCodeProcedure(code + "1", tree.getRightSubtree());
+    }
+  }
 }
--- huffman-coding/PriorityQueue.java
+++ huffman-coding/PriorityQueue.java
@@ -1,50 +1,73 @@
 public class PriorityQueue<E extends Comparable<E>> implements PriorityQueueInterface<E>{
 
-	private E[] items;    //a heap of HuffmanTrees
-	private final static int max_size = 256;
-	private int size;    //number of HuffManTrees in the heap.
-	
-	
-	public PriorityQueue( ) {
-        // constructor which creates an empty heap
-		items = (E[]) new Comparable[max_size];
-		size = 0;
-	}
-	
-	public boolean isEmpty(){
-		return size == 0;
-	}
-	
-	public int getSize(){
-		return size;
-	}
+  private E[] items;    //a heap of HuffmanTrees
+  private final static int max_size = 256;
+  private int size;    //number of HuffManTrees in the heap.
 
-	public E getMin(){
-		E root = null;
-		if (!isEmpty()) root = items[0];
-		return root;
-	}
-	
-	public void add(E newEntry) throws PriorityQueueException{
-	// post: Adds a new entry to the priority queue according to 
-        // the priority value.
-	// ADD YOUR CODE HERE
-	}
- 				
- 	public E removeMin(){
-	// post: Removes the minimum valued item from the PriorityQueue
-		E root = null;
-		if (!isEmpty()){
-			root = items[0];
-			items[0] = items[size-1];
-			size--;
-			heapRebuild(0);
-		}
-		return root;
-	}
-	
-	private void heapRebuild(int root){
-	// Rebuild heap to keep it ordered
-	// ADD YOUR CODE HERE
-	}
+  public PriorityQueue() {
+        // constructor which creates an empty heap
+    items = (E[]) new Comparable[max_size];
+    size = 0;
+  }
+
+  public boolean isEmpty(){
+    return size == 0;
+  }
+
+  public int getSize(){
+    return size;
+  }
+
+  public E getMin() {
+    E root = null;
+    if (!isEmpty()) root = items[0];
+    return root;
+  }
+
+  public void add(E newEntry) throws PriorityQueueException {
+    int curr = size;
+    items[size++] = newEntry;
+    while (curr != 0) {
+      final int parent = curr / 2;
+      if (items[parent].compareTo(items[curr]) > 0) {
+        E temp = items[parent];
+        items[parent] = items[curr];
+        items[curr] = temp;
+        curr = parent;
+      } else {
+        break;
+      }
+    }
+  }
+
+  public E removeMin() {
+  // post: Removes the minimum valued item from the PriorityQueue
+    E root = null;
+    if (!isEmpty()){
+      root = items[0];
+      items[0] = items[size-1];
+      size--;
+      heapRebuild(0);
+    }
+    return root;
+  }
+
+  private void heapRebuild(int root) {
+    int leftChild = 2 * root;
+    int rightChild = 2 * root + 1;
+    if (leftChild >= size) {
+      return;
+    }
+    int smallest = leftChild;
+    if (rightChild < size &&
+        items[leftChild].compareTo(items[rightChild]) > 0) {
+      smallest = rightChild;
+    }
+    if (items[smallest].compareTo(items[root]) < 0) {
+      E temp = items[root];
+      items[root] = items[smallest];
+      items[smallest] = temp;
+      heapRebuild(smallest);
+    }
+  }
 }
