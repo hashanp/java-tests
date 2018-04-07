54d1cf52fe7c0df93741683cd8f9e545ecbaf632
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2018-04-07 13:45:02 +0100

    Fixes indentation for huffman coding

--- huffman-coding/HuffmanData.java
+++ huffman-coding/HuffmanData.java
@@ -2,28 +2,28 @@
 
 public class HuffmanData implements Comparable<HuffmanData>{
 
-	private int frequency;
-	private char symbol;
-	
-	public HuffmanData(int freq, char symb) {
-		this.frequency = freq;
-		this.symbol = symb;	
-	}
-	
-	public HuffmanData(int freq){
-		this.frequency = freq;
-		this.symbol = '\u0000';
-	}
-	
-	public int getFrequency(){
-		return frequency;
-	}
+  private int frequency;
+  private char symbol;
 
-	public char getSymbol(){
-		return symbol;
-	}
-	
-	public int compareTo(HuffmanData entry){
-		return frequency - entry.getFrequency();
-	}
+  public HuffmanData(int freq, char symb) {
+    this.frequency = freq;
+    this.symbol = symb;
+  }
+
+  public HuffmanData(int freq){
+    this.frequency = freq;
+    this.symbol = '\u0000';
+  }
+
+  public int getFrequency(){
+    return frequency;
+  }
+
+  public char getSymbol(){
+    return symbol;
+  }
+
+  public int compareTo(HuffmanData entry){
+    return frequency - entry.getFrequency();
+  }
 }
